efef8fea7571d1411f67120ed5bca35b76cac0ec
Ирек <irek.haibulin@gmail.com>
2022-11-17 23:12:09 +0300

    massives

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < number.length; i++) {
             if (i != 2) {
                 System.out.print(number[i] + ",");
             } else {
@@ -22,7 +22,7 @@ public class Main {
 
         System.out.println();
         double[] fractional = {1.57, 7.654, 9.986};
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < fractional.length; i++) {
             if (i != 2) {
                     System.out.print(fractional[i] + ",");
             } else {
@@ -43,7 +43,7 @@ public class Main {
         arbitrary[0] = 10;
         arbitrary[1] = 20;
         arbitrary[2] = 30;
-        for (int b = 0; b < 3; b++) {
+        for (int b = 0; b < arbitrary.length; b++) {
             if (b != 2) {
                 System.out.print(arbitrary[b] + ",");
             } else {
@@ -58,5 +58,44 @@ public class Main {
                 System.out.println(arbitrary[b]);
             }
         }
+
+        //Задча 4
+        System.out.println();
+        int[] numberNumber = new int[3];
+        numberNumber[0] = 1;
+        numberNumber[1] = 2;
+        numberNumber[2] = 3;
+        for (int i = 0; i < numberNumber.length; i++) {
+            if (i != 2) {
+                if(numberNumber[i] % 2 == 0){
+                    System.out.print(numberNumber[i] + ",");
+                } else {
+                    System.out.print(numberNumber[i]+1 + ",");
+                }
+            } else {
+                if(numberNumber[i] % 2 == 0){
+                    System.out.print(numberNumber[i] + ",");
+                } else {
+                    System.out.print(numberNumber[i]+1);
+                }
+            }
+        }
+        System.out.println("");
+        for (int b = 2; b >= 0; b--) {
+            if (b != 0) {
+                if (numberNumber[b] % 2 == 0) {
+                    System.out.print(numberNumber[b] + ",");
+                }else {
+                    System.out.print(numberNumber[b]+1 + ",");
+                }
+            } else {
+                if(numberNumber[b] % 2 == 0) {
+                    System.out.print(numberNumber[b] + ",");
+                } else{
+                    System.out.print(numberNumber[b]+1);
+                }
+            }
+        }
+
     }
 }
